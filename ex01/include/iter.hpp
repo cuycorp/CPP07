48d2bd845581202ad6f58d1394b9a1693a6ad2fd
cuycorp <cuycorp@gmail.com>
2026-01-25 18:13:26 +0100

    Fixed bug iter template function. Added edge case test files

--- ex01/include/iter.hpp
+++ ex01/include/iter.hpp
@@ -1,10 +1,10 @@
 #ifndef WE_HPP
 # define WE_HPP
 
-template <typename T, typename Func>
-void iter(T *array, const T len, Func func)
+template <typename T, typename T2, typename Func>
+void iter(T *array, const T2 len, Func func)
 {
-    for (int i = 0; i < len ; i++)
+    for (T2 i = 0; i < len ; i++)
     {
         func(array[i]);
     }
